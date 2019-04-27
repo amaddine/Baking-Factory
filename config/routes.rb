391b627374d39cbf53433d1256ff3ab62799a65c
Abhinav Maddineni <amaddine@andrew.cmu.edu>
2019-04-27 16:14:17 -0400

    Got search working

--- config/routes.rb
+++ config/routes.rb
@@ -70,6 +70,7 @@ Rails.application.routes.draw do
   get 'about' => 'home#about', as: :about
   get 'contact' => 'home#contact', as: :contact
   get 'privacy' => 'home#privacy', as: :privacy
+  get 'home/search', to: 'home#search', as: :search
   
   # Set the root url
   root :to => 'home#home'
